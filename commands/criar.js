e73920aabc0b4e7d8914e153c53e320457fb53f0
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2021-06-17 18:53:19 -0300

    Comando de crianção de canais feito

--- commands/criar.js
+++ commands/criar.js
@@ -1,7 +1,25 @@
 module.exports = {
 	name: 'criar',
 	description: 'Cria um canal no servidor',
-	execute(msg) {
-		msg.reply('Canal criado')
+	execute(msg, args) {
+		let tipo = args.shift().toLowerCase()
+		let nome = args.join(' ')
+		let tipo2
+
+		if (tipo === 'text' || tipo === 'texto') {
+			tipo = 'text'
+			tipo2 = 'texto'
+		}
+		else if (tipo === 'voice' || tipo === 'voz' || tipo === 'call') {
+			tipo = 'voice'
+			tipo2 = 'voz'
+		}
+		else {
+			tipo = 'text'
+			tipo2 = 'texto'
+		}
+
+		msg.guild.channels.create(nome, { type: tipo })
+		msg.channel.send(`Canal de ${tipo2} "${nome}" criado`)
 	}
 }
