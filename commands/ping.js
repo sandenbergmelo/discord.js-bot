f947216af331adc7737381ea580c1f4af57184ee
Sandenberg Ferreira Melo <sandenbergferreiramelo@gmail.com>
2021-07-26 07:28:56 -0300

    Refatoração do comando ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,9 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(msg) {
-		let latencia = Date.now() - msg.createdTimestamp
-		msg.reply(`Pong! Essa mensagem foi respondida em ${latencia}ms.`)
+		msg.reply(`Ping...`).then(sent => {
+			const latencia = sent.createdTimestamp - msg.createdTimestamp
+			sent.edit(`Pong! Essa mensagem foi respondida em ${latencia}ms.`)
+		})
 	}
 }
